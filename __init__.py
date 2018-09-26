fbd5c108b881998bac40985fd830f9d5d641647b
David Jaimes <david@djaimes.com>
2018-09-26 00:36:18 -0700

    upload greyfox settings for matplotlib

---
+++ __init__.py
@@ -0,0 +1,38 @@
+"""
+Brindle
+
+https://github.com/astrdj/brindle
+
+A personal collection of functions and styles.
+
+Brindle works with Python 3.6.6.
+
+For more than you ever wanted to know about Brindle, see the
+source code:
+
+https://github.com/astrdj/brindle
+
+If you clone the source code and paste the brindle directory into
+/Users/yourhomedir/anaconda/lib/python3.6/site-packages, it may be
+imported directly, e.g.::
+
+    import brindle as bd
+
+or using ipython::
+
+    ipython
+
+at your terminal, followed by::
+
+    In [1]: import brindle as bd
+
+at the ipython shell prompt.
+
+"""
+
+__author__ = "David Jaimes"
+__version__ = "0.0.0"
+__copyright__ = "Copyright (c) 2010-2018 David Jaimes"
+__license__ = "MIT"
+
+from .mpl_style import greyfox
